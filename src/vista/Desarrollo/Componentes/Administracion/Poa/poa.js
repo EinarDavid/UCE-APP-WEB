e04bf43db81aefcf1c44187f7efd1bfeee321e0a
Einar <einardavidvillarroel@gmail.com>
2021-06-05 22:27:17 -0400

    a

--- src/vista/Desarrollo/Componentes/Administracion/Poa/poa.js
+++ src/vista/Desarrollo/Componentes/Administracion/Poa/poa.js
@@ -26,7 +26,8 @@ class POA extends Component {
       calendarWeekends: true,
       calendarEvents: actividades,
       show: false,
-      actividad: "nada"
+      actividad: "nada",
+      dia: "hoy"
     }
 
   }
@@ -57,50 +58,39 @@ class POA extends Component {
         </div>
         <Modal size="lg" show={this.state.show} onHide={this.handleClose} centered>
 
-          <Form action="#" method="post" >
+         
             <Modal.Header closeButton>
-              <Modal.Title>Registro de Actividades</Modal.Title>
+              <Modal.Title>
+                Actividades del día de {this.state.dia}
+              </Modal.Title>
             </Modal.Header>
+
             <Modal.Body>
               <div>
-                <div className="Form-registro">
-                  <Form.Row>
-                    <Form.Group as={Col} controlId="exampleForm.ControlSelect1">
-                      <Form.Label>Departamento</Form.Label>
-                      <Form.Control as="select" name="departamento">
-                        <option value="juvenil">Soc. Juvenil</option>
-                        <option value="femenil">Soc. Femenil</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="exampleForm.ControlSelect1">
-                      <Form.Label>area</Form.Label>
-                      <Form.Control as="select" name="area">
-                        <option value="ministerial">Ministerial</option>
-                        <option value="accion_social">Accion Social</option>
-                        <option value="ev_min">Evangelismo y Misiones</option>
-                        <option value="edu_cris">Educacion Cristiana</option>
-                        <option value="administracion">Administracion</option>
-                      </Form.Control>
-                    </Form.Group>
-                    <Form.Group as={Col} controlId="formGridPassword">
-                      <Form.Label>Titulo*</Form.Label>
-                      <Form.Control type="text" placeholder="Nombre" name="nombre_actividad" />
-                    </Form.Group>
-                  </Form.Row>
-                  <Form.Row>
-                    <Form.Group as={Col} controlId="formGridEmail">
-                      <Form.Label>Presupuesto</Form.Label>
-                      <Form.Control type="number" placeholder="Presupuesto" name="presupuesto" />
-                    </Form.Group>
-                  </Form.Row>
-                </div>
+                {
+                  this.state.actividad=="nada"?
+                  (
+                    <div>No hay actividades programacas para elste día</div>
+                  )
+                  :
+                  this.state.actividad.map(a=>
+                    {
+                      (
+                        <div>
+                          Titulo: {a.Titulo}
+                          Inicio: {a.Inicio}
+                          Descripcion: {a.Descripcion}
+                        </div>
+                      )
+                    })
+                }
               </div>
             </Modal.Body>
             <Modal.Footer>
               <Button variant="secondary" onClick={this.handleClose}>Close</Button>
               <Button type="submit" variant="outline-primary">Save</Button>
             </Modal.Footer>
-          </Form>
+          
         </Modal>
       </div>
     );
@@ -131,14 +121,16 @@ class POA extends Component {
       {
         this.setState({  // add new event data
           show:true,
-          actividad: actividad
+          actividad: actividad,
+          dia: arg.dateStr
         })
       }
       else
       {
         this.setState({  // add new event data
           show:true,
-          actividad: "nada"
+          actividad: "nada",
+          dia: arg.dateStr
         })
       }    
   }
