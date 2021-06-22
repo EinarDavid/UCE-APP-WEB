48fae6cc1890fc24c28332715b1f76277113de2d
Einar <einardavidvillarroel@gmail.com>
2021-06-21 21:28:01 -0400

    Modificacion

--- src/vista/Desarrollo/Componentes/Administracion/formulario/EditarInformacion.js
+++ src/vista/Desarrollo/Componentes/Administracion/formulario/EditarInformacion.js
@@ -74,31 +74,31 @@ class Formulario extends Component {
                     <Form.Row>
                         <Form.Group as={Col}>
                             <Form.Label>Lunes</Form.Label>
-                            <Form.Control as="text" placeholder="Lunes" name="Horario_Lunes" defaultValue={window.datos.Iglesia.Horario_Lunes} />
+                            <Form.Control type="text" placeholder="Lunes" name="Horario_Lunes" defaultValue={window.datos.Iglesia.Horario_Lunes} />
                         </Form.Group>
                         <Form.Group as={Col}>
                             <Form.Label>Martes</Form.Label>
-                            <Form.Control as="text" placeholder="Martes" name="Horario_Martes" defaultValue={window.datos.Iglesia.Horario_Martes} />
+                            <Form.Control type="text" placeholder="Martes" name="Horario_Martes" defaultValue={window.datos.Iglesia.Horario_Martes} />
                         </Form.Group>
                     </Form.Row>
                     <Form.Row>
                         <Form.Group as={Col}>
                             <Form.Label>Miercoles</Form.Label>
-                            <Form.Control as="text" placeholder="Miercoles" name="Horario_Miercoles" defaultValue={window.datos.Iglesia.Horario_Miercoles} />
+                            <Form.Control type="text" placeholder="Miercoles" name="Horario_Miercoles" defaultValue={window.datos.Iglesia.Horario_Miercoles} />
                         </Form.Group>
                         <Form.Group as={Col}>
                             <Form.Label>Jueves</Form.Label>
-                            <Form.Control as="text" placeholder="Jueves" name="Horario_Jueves" defaultValue={window.datos.Iglesia.Horario_Jueves} />
+                            <Form.Control type="text" placeholder="Jueves" name="Horario_Jueves" defaultValue={window.datos.Iglesia.Horario_Jueves} />
                         </Form.Group>
                     </Form.Row>
                     <Form.Row>
                         <Form.Group as={Col}>
                             <Form.Label>Viernes</Form.Label>
-                            <Form.Control as="text" placeholder="Viernes" name="Horario_Viernes" defaultValue={window.datos.Iglesia.Horario_Viernes} />
+                            <Form.Control type="text" placeholder="Viernes" name="Horario_Viernes" defaultValue={window.datos.Iglesia.Horario_Viernes} />
                         </Form.Group>
                         <Form.Group as={Col}>
                             <Form.Label>Sábado</Form.Label>
-                            <Form.Control as="text" placeholder="Sábado" name="Horario_Sabado" defaultValue={window.datos.Iglesia.Horario_Sabado} />
+                            <Form.Control type="text" placeholder="Sábado" name="Horario_Sabado" defaultValue={window.datos.Iglesia.Horario_Sabado} />
                         </Form.Group>
                     </Form.Row>
                     <Form.Row>
