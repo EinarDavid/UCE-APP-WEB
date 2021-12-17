83a8b0eb09c05972e853b48a4f437718ba01d250
Einar <einardavidvillarroel@gmail.com>
2021-12-17 17:07:12 -0400

    Diseño

--- src/vista/Desarrollo/Componentes/Administracion/menu_adm.js
+++ src/vista/Desarrollo/Componentes/Administracion/menu_adm.js
@@ -179,10 +179,10 @@ class Menu_Admi extends React.Component {
                                 <NavDropdown.Item onClick={this.handleShow_Transferencia}>REG. POR TRANFERENCIAS</NavDropdown.Item>
                                 <NavDropdown.Item onClick={this.handleShow_Solicitud}>REG. POR SOLICITUD</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link onClick={this.handleShow_Usuario} style={menu} className="menu-letra">CAT. PERSONAL</Nav.Link>
+                            <Nav.Link onClick={this.handleShow_Usuario} style={menu} className="menu-letra">REG. PERSONAL</Nav.Link>
                             <NavDropdown className="menu-letra" title="BUSQUEDAS" id="collasible-nav-dropdown">
                                 <NavDropdown.Item onClick={this.handleShow_filtro_membresia}>MEMBRESIA</NavDropdown.Item>
-                                <NavDropdown.Item onClick={this.handleShow_filtro_Usuario}>ENCARGADO</NavDropdown.Item>
+                                <NavDropdown.Item onClick={this.handleShow_filtro_Usuario}>PERSONAL</NavDropdown.Item>
                             </NavDropdown >
                             <Nav.Link onClick={this.handleShow_RegistrarActividad} style={menu} className="menu-letra">REG. ACTIVIDAD</Nav.Link>
                             
