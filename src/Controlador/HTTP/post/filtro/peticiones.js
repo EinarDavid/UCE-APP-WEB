ec14aa8ccdf22f85578ea9b2c8e13a5f9012ed76
Einar <einardavidvillarroel@gmail.com>
2022-01-27 23:57:42 -0400

    Tabla

--- src/Controlador/HTTP/post/filtro/peticiones.js
+++ src/Controlador/HTTP/post/filtro/peticiones.js
@@ -1,4 +1,5 @@
 module.exports = new peticion();
+
 function peticion() {
     this.rutas;
     this.iniciar = (rutas, bd, ver) => {
@@ -45,9 +46,14 @@ function peticion() {
               
                     datos.Iglesia = igle;
                     datos.Iglesia.Logo = (datos.Iglesia.Logo == undefined) ? "Iglesia.png" : datos.Iglesia.Logo;
-              
+                    
+                    var respuestaexcel = clone(respuesta)
+                    respuestaexcel = respuestaexcel.map(a=>{
+                        a.Iglesia = igle.Nombre
+                        return a
+                    })
                     const excel = require('./../../reporte.js');
-                    (excel(respuesta, 'membresia', ['_id','__v', 'Contraseña', 'Hijos', 'FotoPerfil']));
+                    (excel(respuestaexcel, 'membresia', ['_id','__v', 'Contraseña', 'Hijos', 'FotoPerfil', 'Disciplina']));
 
                     res.render("Paginas/index", {
                         pagina: 'filtro',
@@ -161,3 +167,35 @@ function peticion() {
         
     }
 }
+function clone(obj) {
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+        var copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+        var copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = clone(obj[i]);
+        }
+        return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+        var copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = clone(obj[attr]);
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
+
+  }
