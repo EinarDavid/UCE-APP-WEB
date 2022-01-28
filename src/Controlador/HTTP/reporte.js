7d29a438b5e4c4916972fcd432072412bbfc249b
Einar <einardavidvillarroel@gmail.com>
2022-01-27 23:24:13 -0400

    Tabla

--- src/Controlador/HTTP/reporte.js
+++ src/Controlador/HTTP/reporte.js
@@ -15,7 +15,10 @@ module.exports = (datos, nombre, excepciones = [])=>
     var getKeys = function(obj){
       var keys = [];
       for(var key in obj){
-         keys.push(key);
+        if( typeof obj[key] !== 'function') {
+
+          keys.push(key);
+      }
       }
       return keys;
    }
