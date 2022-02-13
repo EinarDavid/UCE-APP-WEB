1ae42e77beff2ad54234f3d8a8450bb02ccb69c5
Einar <einardavidvillarroel@gmail.com>
2022-02-13 14:57:00 -0400

    Reportes

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
@@ -4,6 +4,7 @@ export const FiltroIglesia = () => {
   return (
     <div>
         <h1>Pagina Iglesia filtro</h1>
+        
     </div>
   )
 }
--- src/vista/Desarrollo/Views/filtroIglesia.js
+++ src/vista/Desarrollo/Views/filtroIglesia.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Menu from '../Componentes/Menus/MenuAdministrador';
 import { FiltroIglesia } from '../Componentes/Administracion/filtros/FiltroIglesia';
 import Menu_Admi from '../Componentes/Administracion/menu_adm'
 import Fondo from '../Componentes/Administracion_vista/Backgroud/backgroud';
@@ -8,7 +9,7 @@ class filtroIglesia extends React.Component {
         return (
 
             <React.Fragment>
-                {/* <Menu_Admi/> */}
+                <Menu/>
                 {/* <Fondo /> */}
                 <FiltroIglesia />
 
