5ccaf01c089ff1a06de6f7c6aab37fd98e65b4f7
Einar <einardavidvillarroel@gmail.com>
2022-02-14 16:15:23 -0400

    Reportes

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Form, Col, Button } from 'react-bootstrap';
 
 import './FiltroIglesia.css';
 
@@ -37,14 +37,30 @@ export const FiltroIglesia = () => {
                     </div>
                 </div>
                 <br />
-                
+
                 <div className='SearchReporte'>
+                    <Form>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridEmail">
+                                <Form.Label>Ingresa el dato de Busqueda</Form.Label>
+                                <Form.Control type="search" placeholder="Ej. Villarroel" className='SearchTextInput' required />
+                            </Form.Group>
+                            {/* <Button variant="primary" type="submit">Submit</Button> */}
+                            <Button type="submit" className='ButtonReporte'>BUSCAR</Button>
+                        </Form.Row>
+                    </Form>
+
+
+                </div>
+
+                <div className='SearchReporte'>
+                    <br />
                     <input type='search' className='SearchTextInput' placeholder='Ej. Villarroel'></input>
                     <button type='submit' className='ButtonReporte' > BUSCAR </button>
+                    <br />
                 </div>
-                <br />
 
-                <div className="Form-filtro2">
+                <div className='Form-filtro2'>
                     <Table responsive striped hover>
                         <thead>
                             <tr>
