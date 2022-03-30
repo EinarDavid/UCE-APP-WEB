3607a189e402712a2cf9f7acef7a2d5ec63f7c73
Einar <einardavidvillarroel@gmail.com>
2022-03-30 18:52:13 -0400

    Ultimas modificaciones

--- src/vista/Desarrollo/Componentes/Administracion/menu_adm.js
+++ src/vista/Desarrollo/Componentes/Administracion/menu_adm.js
@@ -180,10 +180,8 @@ class Menu_Admi extends React.Component {
                                 <NavDropdown.Item onClick={this.handleShow_Solicitud}>REG. POR SOLICITUD</NavDropdown.Item>
                             </NavDropdown>
                             <Nav.Link onClick={this.handleShow_Usuario} style={menu} className="menu-letra">REG. PERSONAL</Nav.Link>
-                            <NavDropdown className="menu-letra" title="BUSQUEDAS" id="collasible-nav-dropdown">
-                                <NavDropdown.Item onClick={this.handleShow_filtro_membresia}>MEMBRESIA</NavDropdown.Item>
-                                
-                            </NavDropdown >
+                            <Nav.Link onClick={this.handleShow_filtro_membresia} style={menu} className="menu-letra">REPORTE MEMBRESIA</Nav.Link>
+                            
                             <Nav.Link onClick={this.handleShow_RegistrarActividad} style={menu} className="menu-letra">REG. ACTIVIDAD</Nav.Link>
                             
                             <Nav.Link href={'/Iglesia/'+ window.datos.Iglesia._id +'/poa'} style={menu} className="menu-letra">CALENDARIO DE ACT.</Nav.Link>
