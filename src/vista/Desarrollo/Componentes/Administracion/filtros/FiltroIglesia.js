632061f379c515800c11c674f078f37aa373d9c6
Einar <einardavidvillarroel@gmail.com>
2022-04-05 19:40:09 -0400

    DB

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
@@ -250,19 +250,20 @@ export const FiltroIglesia = () => {
                         </div>
 
                     </div>
+                    <div style={{height: '15px'}}/>
                     <div className='CardContendMiembro'>
                         <div className='CardMiembroPor'>
-                            <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                            {/* <img src='./Icons/Icons.png' className='ImageCardReporte' ></img> */}
                             <p className='ParrafoCardReporte'>Total de miembros por Bautizo</p>
                             <h1 className='NumCardReporte'>{contBautizo}</h1>
                         </div>
                         <div className='CardMiembroPor'>
-                            <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                            {/* <img src='./Icons/Icons.png' className='ImageCardReporte' ></img> */}
                             <p className='ParrafoCardReporte'>Total de miembros por Transferencia</p>
                             <h1 className='NumCardReporte'>{contTransferencia}</h1>
                         </div>
                         <div className='CardMiembroPor'>
-                            <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                            {/* <img src='./Icons/Icons.png' className='ImageCardReporte' ></img> */}
                             <p className='ParrafoCardReporte'>Total de miembros por Solicitud</p>
                             <h1 className='NumCardReporte'>{contSolicitud}</h1>
                         </div>
