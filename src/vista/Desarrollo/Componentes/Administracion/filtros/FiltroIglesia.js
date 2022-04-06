50bcb4a4192a6785cb1fc9e3b5128ad4c22937f6
Einar <einardavidvillarroel@gmail.com>
2022-04-05 22:22:03 -0400

    Dashboard

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroIglesia.js
@@ -272,7 +272,7 @@ export const FiltroIglesia = () => {
                         </div>
                     </div>
                     <div className='GraficoContend'>
-                        <LineChart width={600} height={300} data={iglesia} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                        <LineChart width={'100%'} height={'100%'} data={iglesia} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                             <Line type="monotone" dataKey="miembros" stroke="#8884d8" />
                             <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
                             <XAxis dataKey="Nombre" />
