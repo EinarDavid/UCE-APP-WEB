003fcc3a39a99b3c8f520ea34458eacf7c73e221
Einar <einardavidvillarroel@gmail.com>
2022-04-20 17:08:43 -0400

    Reporte Membresias

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroMembresias.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroMembresias.js
@@ -4,16 +4,32 @@ import './FiltroIglesia.css';
 export const FiltroMembresias = () => {
 
     const membresias = window.datos.membresias;
-    
+
     console.log('----------', membresias);
+    console.log('User....', window.datos.user)
+
+    var TotalMiembros= 0;
+
+    const contarMiembros = () => {
+
+        var contador = 0;
+        membresias.forEach(miembro => {
+            if (window.datos.user._id === miembro.Iglesia) {
+                contador += 1;
+            }
+        });
+        TotalMiembros = contador;
+
+    }
+    // contarMiembros();
 
     return (
         <div>
-            <br/>
-            <br/>
+            <br />
+            <br />
             <h1 className='TituloGraficos'>Registros</h1>
             <div className='CardContend'>
-            <div className='CardReporte4'>
+                <div className='CardReporte4'>
                     <img src='../../Icons/Icons.png' className='ImageCardReporte' ></img>
                     <p className='ParrafoCardReporte'>Total de miembros registrados</p>
                     <h1 className='NumCardReporte'>10</h1>
