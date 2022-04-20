76465353fb92b258bc648e59d1ae6232b3deffb8
Einar <einardavidvillarroel@gmail.com>
2022-04-20 16:45:58 -0400

    Reporte Membresias

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroMembresias.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroMembresias.js
@@ -4,26 +4,27 @@ import './FiltroIglesia.css';
 export const FiltroMembresias = () => {
     return (
         <div>
+            <br/>
             <h1 className='TituloGraficos'>Registros</h1>
             <div className='CardContend'>
                 <div className='CardReporte4'>
-                    <img src='../Icons/Icons.png' className='ImageCardReporte' ></img>
+                    <img src='../../Icons/Icons.png' className='ImageCardReporte' ></img>
                     <p className='ParrafoCardReporte'>Total de Iglesias registradas</p>
                     <h1 className='NumCardReporte'>10</h1>
                 </div>
 
                 <div className='CardReporte4'>
-                    <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                    <img src='../../Icons/Icons.png' className='ImageCardReporte' ></img>
                     <p className='ParrafoCardReporte'>Total de miembros registrados</p>
                     <h1 className='NumCardReporte'>10</h1>
                 </div>
                 <div className='CardReporte4'>
-                    <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                    <img src='../../Icons/Icons.png' className='ImageCardReporte' ></img>
                     <p className='ParrafoCardReporte'>Total de miembros registrados</p>
                     <h1 className='NumCardReporte'>10</h1>
                 </div>
                 <div className='CardReporte4'>
-                    <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                    <img src='../../Icons/Icons.png' className='ImageCardReporte' ></img>
                     <p className='ParrafoCardReporte'>Total de miembros registrados</p>
                     <h1 className='NumCardReporte'>10</h1>
                 </div>
