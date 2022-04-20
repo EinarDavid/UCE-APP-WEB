0e7447d8a6d301bbee3c416c7308659d6bee5e88
Einar <einardavidvillarroel@gmail.com>
2022-04-20 16:43:16 -0400

    Reporte Membresias

--- src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroMembresias.js
+++ src/vista/Desarrollo/Componentes/Administracion/filtros/FiltroMembresias.js
@@ -7,7 +7,7 @@ export const FiltroMembresias = () => {
             <h1 className='TituloGraficos'>Registros</h1>
             <div className='CardContend'>
                 <div className='CardReporte4'>
-                    <img src='./Icons/Icons.png' className='ImageCardReporte' ></img>
+                    <img src='../Icons/Icons.png' className='ImageCardReporte' ></img>
                     <p className='ParrafoCardReporte'>Total de Iglesias registradas</p>
                     <h1 className='NumCardReporte'>10</h1>
                 </div>
