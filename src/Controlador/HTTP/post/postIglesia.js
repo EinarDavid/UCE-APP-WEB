42da2aef420a31c640bebdd27045c3d7d778c417
EinarDavid <einardavidvillarroel@gmail.com>
2023-06-29 14:43:09 -0400

    Socjets

--- src/Controlador/HTTP/post/postIglesia.js
+++ src/Controlador/HTTP/post/postIglesia.js
@@ -136,7 +136,7 @@ function peticion() {
                             )
                             bd.cruds.crudIglesia.modificar(igle._id, igle, () => {
                                 console.log(igle.Actividades)
-                                notificarActividadNueva(actividad, "todos")
+                                notificarActividadNueva(io, actividad, "todos")
                             })
                         });
                         res.redirect("/admiCental");
@@ -182,7 +182,7 @@ function peticion() {
                         )
                         bd.cruds.crudIglesia.modificar(req.user.Iglesia, iglesia, () => {
                             //console.log(iglesia.Actividades)
-                            notificarActividadNueva(actividad, iglesia._id)
+                            notificarActividadNueva(io, actividad, iglesia._id)
                             res.redirect("back")
                         })
                     })
@@ -223,6 +223,6 @@ function peticion() {
     }
 }
 
-function notificarActividadNueva(actividad, iglesia){
+function notificarActividadNueva(io, actividad, iglesia){
     io.sockets.emit("nuevaActividad:::"+iglesia, actividad);
 }
